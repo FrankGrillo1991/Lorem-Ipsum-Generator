eda009449d2dd8dd9b5e49d5a574e174699e220d
FrankGrillo1991 <fsg12491@aol.com>
2024-08-03 02:59:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -108,4 +108,48 @@ function generateText(
     return loremIpsumArray.join("\n");
 }
 
+// Function to generate a specified number of words
+function generateWords(numWords) {
 
+    // Lorem Ipsum text for demonstration purposes
+    const loremIpsumText = 
+    `Lorem ipsum dolor sit amet, consectetur
+    adipsiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna
+    aliqua.  Diam in arcu cursus euismod 
+    quis viverra nibh. Nunc aliquet bibendum
+    enim facilisis gravida neque convallis
+    a cras.  Sagittis purus sit amet volutpat
+    Consequat mauris.  Duis ultricies lacus
+    sed turpis ticidunt id.  Consequat interdum
+    varius sit amet mattis vulputate/  Enim sed 
+    faucibus turpis in eu.  Ridiculus mus mauris 
+    vitae ultricies leo integer malesuada nunc vel.
+    Nulla pharetra diam sit amet nisl molestie nunc
+    non blandit massa enim.  Dis parturient montes
+    nascetur ridiculus mus.  Justo nec ultricies dui 
+    sapien eget.  Enim tortur at auctor urna nunc.
+    Dictumst quisque sagittis purus sit amet volutpat
+    consequat mauris nunc.`;
+
+    // Split the Lorem Ipsum text into words
+    const words = 
+    loremIpsumText.split(" ");
+
+    // Ensure the number of words requested is
+    // within the bounds of the available words
+    if (numWords <= words.length) {
+        return words.slice(0, numWords)
+        .join.apply(" ")
+    } else {
+        return words.join(" ");
+    }
+}
+
+// Display Lorem Ipsum text
+function displayLoremIpsum(text) {
+    outputContainer.innerHTML = text;
+}
+
+// Initialize the app
+createOptionsUI();
